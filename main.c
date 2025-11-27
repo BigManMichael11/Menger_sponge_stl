8e895a6d2de9b33fb0713ae2e8cacfbceec8a3e7
Michael Anderson <michael.anderson0611@gmail.com>
2025-11-27 00:46:03 -0500

    I think it works.

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void generate_triangles_from_square(int points[4][3]){
     int left = 3;
     int right = 2;
     for(int i = 1; i < 4; i++){
-        // printf("%d %d\n", (abs(points[0][0] - points[i][0]) + abs(points[0][1] - points[i][1]) + abs(points[0][2] - points[i][2])), (abs(points[0][0] - points[op][0]) + abs(points[0][1] - points[op][1]) + abs(points[0][2] - points[op][2])));
+        // printf("%d %d\r\n", (abs(points[0][0] - points[i][0]) + abs(points[0][1] - points[i][1]) + abs(points[0][2] - points[i][2])), (abs(points[0][0] - points[op][0]) + abs(points[0][1] - points[op][1]) + abs(points[0][2] - points[op][2])));
         if((abs(points[0][0] - points[i][0]) + abs(points[0][1] - points[i][1]) + abs(points[0][2] - points[i][2])) >
         (abs(points[0][0] - points[op][0]) + abs(points[0][1] - points[op][1]) + abs(points[0][2] - points[op][2]))){
             op = i;
@@ -28,20 +28,20 @@ void generate_triangles_from_square(int points[4][3]){
         }
     }
 
-    // printf("Close triange: %d %d %d\n", points[0][0], points[0][1], points[0][2]);
-    // printf("Far triange: %d %d %d\n", points[op][0], points[op][1], points[op][2]);
-    // printf("op: %d Left: %d Right:%d\n", op, left, right);
-    fprintf(fptr,"     facet normal 0 0 0\n         outer loop\n");
-    fprintf(fptr,"             vertex %d %d %d\n",points[0][0], points[0][1], points[0][2]);
-    fprintf(fptr,"             vertex %d %d %d\n",points[left][0], points[left][1], points[left][2]);
-    fprintf(fptr,"             vertex %d %d %d\n",points[right][0], points[right][1], points[right][2]);
-    fprintf(fptr,"         endloop\n     endfacet\n");
+    // printf("Close triange: %d %d %d\r\n", points[0][0], points[0][1], points[0][2]);
+    // printf("Far triange: %d %d %d\r\n", points[op][0], points[op][1], points[op][2]);
+    // printf("op: %d Left: %d Right:%d\r\n", op, left, right);
+    fprintf(fptr,"     facet normal 0 0 0\r\n         outer loop\r\n");
+    fprintf(fptr,"             vertex %d %d %d\r\n",points[0][0], points[0][1], points[0][2]);
+    fprintf(fptr,"             vertex %d %d %d\r\n",points[left][0], points[left][1], points[left][2]);
+    fprintf(fptr,"             vertex %d %d %d\r\n",points[right][0], points[right][1], points[right][2]);
+    fprintf(fptr,"         endloop\r\n     endfacet\r\n");
 
-    fprintf(fptr,"     facet normal 0 0 0\n         outer loop\n");
-    fprintf(fptr,"             vertex %d %d %d\n",points[op][0], points[op][1], points[op][2]);
-    fprintf(fptr,"             vertex %d %d %d\n",points[left][0], points[left][1], points[left][2]);
-    fprintf(fptr,"             vertex %d %d %d\n",points[right][0], points[right][1], points[right][2]);
-    fprintf(fptr,"         endloop\n     endfacet\n");
+    fprintf(fptr,"     facet normal 0 0 0\r\n         outer loop\r\n");
+    fprintf(fptr,"             vertex %d %d %d\r\n",points[op][0], points[op][1], points[op][2]);
+    fprintf(fptr,"             vertex %d %d %d\r\n",points[left][0], points[left][1], points[left][2]);
+    fprintf(fptr,"             vertex %d %d %d\r\n",points[right][0], points[right][1], points[right][2]);
+    fprintf(fptr,"         endloop\r\n     endfacet\r\n");
 
     return;
 }
@@ -56,10 +56,10 @@ void generate_square(const int center[3], const int width, const int axis[3], in
 
     memcpy(points, points_local, sizeof(points_local));
 
-    // printf("point1: %d, %d, %d\n", points[0][0], points[0][1], points[0][2]);
-    // printf("point2: %d, %d, %d\n", points[1][0], points[1][1], points[1][2]);
-    // printf("point3: %d, %d, %d\n", points[2][0], points[2][1], points[2][2]);
-    // printf("point4: %d, %d, %d\n", points[3][0], points[3][1], points[3][2]);
+    // printf("point1: %d, %d, %d\r\n", points[0][0], points[0][1], points[0][2]);
+    // printf("point2: %d, %d, %d\r\n", points[1][0], points[1][1], points[1][2]);
+    // printf("point3: %d, %d, %d\r\n", points[2][0], points[2][1], points[2][2]);
+    // printf("point4: %d, %d, %d\r\n", points[3][0], points[3][1], points[3][2]);
 
 
     if(gen_triangle) generate_triangles_from_square(points);
@@ -96,7 +96,7 @@ int all_points_arr[ALL_POINTS_SIZE][4];
 
 void print_last_arr(int arr[LAST_POINTS_SIZE][4], int length){
     for(int i = 0; i < length; i++){
-        printf("Last%d: %d %d %d %d\n", i, arr[i][0], arr[i][1], arr[i][2], arr[i][3]);
+        printf("Last%d: %d %d %d %d\r\n", i, arr[i][0], arr[i][1], arr[i][2], arr[i][3]);
     }
 }
 
@@ -130,10 +130,10 @@ void populate_michael_sponge(int iterations){
             int local_cube[8][3];
             int width = 2 * 3 * (my_pow(3, iterations - i));
             int center[3] = {last_points_arr[j][0],last_points_arr[j][1],last_points_arr[j][2]};
+            // printf("Making cube at %d,%d,%d with size %d\r\n", center[0], center[1], center[2], width);
             generate_cube(center, width, false, local_cube);
             for(int k = 0; k < 8; k++){
                 local_last_arr[local_last_idx][0] = local_cube[k][0];
-                // printf("CUbe: %d %d %d\n", local_cube[k][0], local_cube[k][1], local_cube[k][2]);
                 local_last_arr[local_last_idx][1] = local_cube[k][1];
                 local_last_arr[local_last_idx][2] = local_cube[k][2];
                 local_last_arr[local_last_idx][3] = i;
@@ -141,6 +141,7 @@ void populate_michael_sponge(int iterations){
             }
         }
         memcpy(last_points_arr, local_last_arr, sizeof(last_points_arr));
+        last_points_idx = local_last_idx;
         memcpy(&all_points_arr[all_points_idx], local_last_arr, local_last_idx * sizeof(local_last_arr[0]));
         all_points_idx += local_last_idx;
     }
@@ -154,7 +155,7 @@ void populate_michael_sponge(int iterations){
         // int center[3] = {0,0,0};
         int michael_idx = iterations - all_points_arr[i][3] - 1;
         int width = 2 * (my_pow(3, michael_idx));
-        printf("Generating cube at %d,%d,%d with size %d\n", center[0], center[1], center[2], width);
+        // printf("Generating cube at %d,%d,%d with size %d\r\n", center[0], center[1], center[2], width);
         generate_cube(center, width, true, cube_points);
         // generate_cube(center, 6);
     }
@@ -163,13 +164,13 @@ void populate_michael_sponge(int iterations){
 int main() {
     fptr = fopen("cube.stl", "w");
     if (fptr == NULL) {
-        printf("Error opening file!\n");
+        printf("Error opening file!\r\n");
         return 1;
     }
 
-    fprintf(fptr,"solid name\n");
+    fprintf(fptr,"solid name\r\n");
     // generate_cube(0, 2);
     populate_michael_sponge(4);
-    fprintf(fptr,"endsolid name\n");
+    fprintf(fptr,"endsolid name\r\n");
     return 0;
 }
